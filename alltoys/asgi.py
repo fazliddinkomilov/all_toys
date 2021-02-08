c7d0827506f0418e52386e9f80c81eddc5caca97
fazliddinkomilov <fazliddinkomilov777@gmail.com>
2021-02-08 16:36:56 +0500

    2

--- alltoys/asgi.py
+++ alltoys/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for alltoys project.
+ASGI config for alltoys123 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alltoys.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alltoys123.settings')
 
 application = get_asgi_application()
