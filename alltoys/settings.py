06d78dd07253a9522f31800ab9bc8dc72b311f7f
fazliddinkomilov <fazliddinkomilov777@gmail.com>
2021-02-12 15:49:05 +0500

    created models

--- alltoys/settings.py
+++ alltoys/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
     'toys',
     'company'
 ]
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'alltoys.urls'
@@ -131,3 +129,4 @@ INTERNAL_IPS = [
     '127.0.0.1',
     # ...
 ]
+
